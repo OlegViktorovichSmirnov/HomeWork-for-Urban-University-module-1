4076fe8bc0772138c244d9ec55f1ceecb9bb712e
Олег <solegva@ya.ru>
2024-10-21 11:07:10 +0300

    21/10 + m_1_4

--- module_1_4.py
+++ module_1_4.py
@@ -13,4 +13,4 @@ course = 'Python'
 number_of_finished_tasks = 12
 number_of_used_hours = 1.5
 time_on_each_task = number_of_used_hours/number_of_finished_tasks
-print('Курс:', course, ',', 'Количество законченных заданий:', number_of_finished_tasks, ',', 'Количество затраченных часов:', number_of_used_hours, ',', 'Время на одно задание', time_on_each_task)
+print('Курс:', course + ',', 'Количество законченных заданий:', number_of_finished_tasks, ',', 'Количество затраченных часов:', number_of_used_hours, ',', 'Время на одно задание', time_on_each_task)
