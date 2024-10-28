df5cb886d5c55bfdb0e48aadcccda8816bc67932
Олег <solegva@ya.ru>
2024-10-28 06:06:17 +0300

    28/10 + m_1_2

--- module_1_2.py
+++ module_1_2.py
@@ -13,4 +13,8 @@ course = 'Python'
 number_of_finished_tasks = 12
 number_of_used_hours = 1.5
 time_on_each_task = number_of_used_hours/number_of_finished_tasks
-print('Курс:', course + ',', 'Количество законченных заданий:', number_of_finished_tasks, ',', 'Количество затраченных часов:', number_of_used_hours, ',', 'Время на одно задание', time_on_each_task)
+
+# Так как вывод состоит из разных типов данных, для того чтобы был красивый вывод с запятой после окончания описания
+# каждого элемента "Курс: Python," используем f строку, значения помещаем в фигурные скобки
+
+print(f'Курс: {course}, Количество законченных заданий: {number_of_finished_tasks}, Количество затраченных часов: {number_of_used_hours}, Время на одно задание: {time_on_each_task}')
